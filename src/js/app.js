851b9ad087206ef9943f0dad7ea1010954fe5ad8
Svetlana Ryabova <ganyim@inbox.ru>
2020-09-09 21:47:05 +0500

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,13 @@ import data from './data.js';
 import add from './add.js';
 
 const table = document.querySelector('table');
-data.forEach( (element) => {
-    table.insertAdjacentHTML('beforeend', add(element));
-})
+data.forEach((element) => {
+  table.insertAdjacentHTML('beforeend', add(element));
+});
 
 const tr = document.querySelector('title');
 tr.querySelectorAll('td').forEach((element) => {
-    element.addEventListener('click', () => {
-       element.insertAdjacentText('afterend', '&#8595');
-    })
-})
+  element.addEventListener('click', () => {
+    element.insertAdjacentText('afterend', '&#8595');
+  });
+});
