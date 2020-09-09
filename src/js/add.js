cdfddea978ce52302c482d5c29abc4b14ecb56da
Svetlana Ryabova <ganyim@inbox.ru>
2020-09-10 01:24:36 +0500

    fixed

--- src/js/add.js
+++ src/js/add.js
@@ -2,3 +2,15 @@ export default function add(data) {
   const html = `<tr data-id=${data.id} data-title=${data.title} data-year=${data.year} data-imdb=${data.imdb}><td>${data.id}</td><td>${data.title}</td><td>${data.year}</td><td>imdb: ${data.imdb.toFixed(2)}</td></tr>`;
   return html;
 }
+
+export function addtitle() {
+  const html = "<tr class = 'title'><td>id</td><td>title</td><td>year</td><td>imdb</td></tr>";
+  return html;
+}
+
+export function remove() {
+  const tr = document.querySelectorAll('tr');
+  for (let i = 1; i < tr.length; i++) {
+    tr.item(i).remove();
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,70 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable prefer-template */
+/* eslint-disable no-param-reassign */
+/* eslint-disable quotes */
 import data from './data.js';
-import add from './add.js';
+import add, { addtitle, remove } from './add.js';
 
 const table = document.querySelector('table');
+table.insertAdjacentHTML('beforeend', addtitle());
 data.forEach((element) => {
   table.insertAdjacentHTML('beforeend', add(element));
 });
 
-const tr = document.querySelector('title');
-tr.querySelectorAll('td').forEach((element) => {
+const td = Array.from(document.querySelectorAll('.title td'));
+const array = [];
+
+// array названия заголовков таблицы
+td.forEach((element) => {
+  array.push(element.innerText);
+});
+// клик по заголовкам таблицы
+td.forEach((element) => {
   element.addEventListener('click', () => {
-    element.insertAdjacentText('afterend', '&#8595');
+    for (let i = 0; i < array.length; i++) {
+      td[i].innerText = array[i];
+      td[i].classList.remove('check');
+    }
+    if (td.indexOf(element) === 0) {
+      data.sort((a, b) => a.id - b.id);
+      remove();
+      data.forEach((el) => {
+        table.insertAdjacentHTML('beforeend', add(el));
+      });
+    }
+    if (td.indexOf(element) === 1) {
+      data.sort((a, b) => {
+        if (a.title > b.title) {
+          return 1;
+        }
+        if (a.title < b.title) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+      });
+      remove();
+      data.forEach((el) => {
+        table.insertAdjacentHTML('beforeend', add(el));
+      });
+    }
+
+    if (td.indexOf(element) === 2) {
+      data.sort((a, b) => a.year - b.year);
+      remove();
+      data.forEach((el) => {
+        table.insertAdjacentHTML('beforeend', add(el));
+      });
+    }
+    if (td.indexOf(element) === 3) {
+      data.sort((a, b) => parseFloat(a.imdb) - parseFloat(b.imdb));
+      remove();
+      data.forEach((el) => {
+        table.insertAdjacentHTML('beforeend', add(el));
+      });
+    }
+    const text = element.innerText;
+    element.innerText = text + ' ' + String.fromCharCode(8659);
+    element.classList.add('check');
   });
 });
